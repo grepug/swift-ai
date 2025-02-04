33213640c3c8b4e23a5933a796c413f94c64c3bc
Kai Shao <grepug@icloud.com>
2025-02-04 11:51:23 +0800

    Rename DeepSeekSF to SiliconFlow and update version handling to use a more descriptive enum

--- Sources/Core/Models/DeepSeek.swift
+++ Sources/Core/Models/DeepSeek.swift
@@ -12,26 +12,32 @@ public struct DeepSeek: LLMModel {
     }
 }
 
-public struct DeepSeekSF: LLMModel {
+public struct SiliconFlow: LLMModel {
     public let name: String
     public let baseURL: URL
     public let apiKey: String
 
-    public enum Version: String {
-        case v2_5
-        case v3
+    public enum Name {
+        case deepSeek_2_5
+        case deepSeek_3
+        case meta
+        case qwen
+        case custom(String)
 
-        var name: String {
+        var rawValue: String {
             switch self {
-            case .v2_5: "deepseek-ai/DeepSeek-V2.5"
-            case .v3: "deepseek-ai/DeepSeek-V3"
+            case .deepSeek_2_5: "deepseek-ai/DeepSeek-V2.5"
+            case .deepSeek_3: "deepseek-ai/DeepSeek-V3"
+            case .meta: "meta-llama/Llama-3.3-70B-Instruct"
+            case .qwen: "Qwen/Qwen2.5-14B-Instruct"
+            case .custom(let name): name
             }
         }
     }
 
-    public init(apiKey: String, version: Version? = .v2_5, baseURL: URL? = nil) {
+    public init(apiKey: String, name: Name? = nil, baseURL: URL? = nil) {
         self.apiKey = apiKey
-        self.name = (version ?? .v2_5).name
+        self.name = name?.rawValue ?? Name.deepSeek_2_5.rawValue
         self.baseURL = baseURL ?? URL(string: "https://api.siliconflow.cn/v1")!
     }
 }
