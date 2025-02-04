18e6e7d79f5cb88f48b5e81af0b673b02ebbdcc2
Kai Shao <grepug@icloud.com>
2025-02-04 16:47:37 +0800

    Remove LLMRunnerMiddleware, LLMClient, and Client implementations to streamline the codebase

--- Package.swift
+++ Package.swift
@@ -13,34 +13,15 @@ let package = Package(
         .library(
             name: "SwiftAI",
             targets: ["SwiftAI"]
-        ),
-        .library(
-            name: "SwiftAIVapor",
-            targets: ["SwiftAIVapor"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/grepug/event-source.git", branch: "master"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.6.1"),
-        .package(url: "https://github.com/grepug/concurrency-utils.git", branch: "main"),
+        .package(url: "https://github.com/grepug/event-source.git", branch: "master")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftAI",
             path: "Sources/Core"
-        ),
-        .target(
-            name: "SwiftAIVapor",
-            dependencies: [
-                "SwiftAI",
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Dependencies", package: "swift-dependencies"),
-                .product(name: "ConcurrencyUtils", package: "concurrency-utils"),
-            ],
-            path: "Sources/SwiftAIVapor"
         ),
         .testTarget(
             name: "swift-aiTests",
